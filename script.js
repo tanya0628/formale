14b30c133a0a733ccb66cda9d21ae23dff9c2bd6
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-12-11 14:49:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.querySelector('.menu-btn');
+  const navbar = document.querySelector('.navbar');
+
+  menuBtn.onclick = () => {
+    navbar.classList.toggle('active');
+  };
+
